e05a719eaf72d780b69262e7278b22245add5679
Andrei <andrei@fullview.so>
2022-10-19 17:46:37 +0300

    chore: upgraded configuration and changed footer

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -18,15 +18,23 @@ export const Footer: FC = () => {
         }}
       >
         <Text style={{ color: "#eff2e2" }}>{"{ "} </Text>
-        <Text style={{ color: "#78dde8" }}>Andrei Lucaci</Text>{" "}
-        <Text style={{ color: "#ff6189" }}>©2021</Text>{" "}
+        <Link
+          style={{ color: "#78dde8" }}
+          title="andreilucaci.ro"
+          href="https://andreilucaci.ro"
+        >
+          Andrei Lucaci
+        </Link>{" "}
+        <Text style={{ color: "#ff6189" }}>
+          ©2021-${new Date().getFullYear()}
+        </Text>{" "}
         <Text style={{ color: "#eff2e2" }}>-</Text>{" "}
         <Link
           style={{ color: "#a9dc76" }}
-          title="github"
-          href="https://github.com/AndreiLucaci/json-format"
+          title="jsonf"
+          href="https://jsonf.io"
         >
-          https://github.com/AndreiLucaci/json-format
+          https://jsonf.io
         </Link>{" "}
         <Text style={{ color: "#eff2e2" }}>-</Text>{" "}
         <Text style={{ color: "#78dde8" }}>JSON</Text>{" "}
